a0be4749fee6714d919ed9c696bc0554ab3552fe
Hunter Davis <hunterdavis730@gmail.com>
2019-09-05 23:49:31 -0600

    right before adding in material ui files

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,9 @@ import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import footerStyles from "./footer.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLinkedin, faGithub } from "@fortawesome/fontawesome-svg-core"
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => {
-  console.log(faGithub)
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -20,8 +19,23 @@ const Footer = () => {
       <hr></hr>
       <footer className={footerStyles.footer}>
         <p>Created by {data.site.siteMetadata.author}, © 2019</p>
-        <FontAwesomeIcon icon={["fab", "fa-github"]} />
-        <FontAwesomeIcon icon="fa-linkedin-in" />
+        <div>
+          <a href="https://github.com/hunterdavis730" target="_blank">
+            <FontAwesomeIcon
+              className={footerStyles.footerIcon}
+              icon={faGithub}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hunter-davis-ba2008178/"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              className={footerStyles.footerIcon}
+              icon={faLinkedinIn}
+            />
+          </a>
+        </div>
       </footer>
     </>
   )
